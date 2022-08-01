6158c4fa543546b22c66d496bc9e82878062f991
SushiMeliodas <bryanyf@hotmail.com>
2022-08-01 11:50:36 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useMemo, createContext } from 'react';
+
+// import Box from '@mui/material/Box';
+import { Box } from '@mui/material';
+import Toolbar from '@mui/material/Toolbar';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import Grow from '@mui/material/Grow';
+
+// import './App.css';
+import NavBar from './components/layout/NavBar';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mode, setMode] = useState<'light' | 'dark'>('light');
+
+  const toggleColorMode = () => {
+    setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+  };
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box sx={{ display: 'flex' }}>
+          <NavBar toggleColorMode={toggleColorMode} />
+          <Box component='main' sx={{ p: 3 }}>
+            <Toolbar />
+            Inside
+          </Box>
+        </Box>
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
